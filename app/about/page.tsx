aac288368debff7cb296e85c65da7677c88f1adb
Rohitkumar5205 <rohit@arogyamantra.com>
2026-01-31 12:57:54 +0530

    about page working

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AboutJoin from "@/components/about/AboutJoin";
+import AboutNamoGange from "@/components/about/AboutNamoGange";
+const page = () => {
+  return (
+    <div>
+      <AboutNamoGange/>
+      <AboutJoin/>
+    </div>
+  )
+}
+
+export default page
